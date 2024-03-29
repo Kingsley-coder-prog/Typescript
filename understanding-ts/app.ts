f82c98d6196f67da2aa80087e0c48fb9db54d3c0
BlockGem <kifeanyi216@gmail.com>
2024-03-29 13:23:31 +0100

    types in ts

---
+++ understanding-ts/app.ts
@@ -0,0 +1,11 @@
+console.log("Your code goes here...");
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const num1 = "5";
+const num2 = 2.8;
+
+const result = add(+num1, num2);
+console.log(result);
