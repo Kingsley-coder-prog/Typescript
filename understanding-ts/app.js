813fdf70579e10f369e658c3174ef923a7332d11
BlockGem <kifeanyi216@gmail.com>
2024-03-30 12:28:28 +0100

    Function Return Types and Void

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,21 +1,7 @@
-function combine(n1, n2, resultConversion) {
-    var result;
-    if ((typeof n1 === "number" && typeof n2 === "number") || resultConversion === "as-number") {
-        result = +n1 + +n2;
-    }
-    else {
-        result = n1.toString() + n2.toString();
-    }
-    return result;
-    //   if (resultConversion === "as-number") {
-    //     return +result;
-    //   } else {
-    //     return result.toString();
-    //   }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-var combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-var combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+function printResult(num) {
+    console.log("Result is: ", num);
+}
+printResult(add(10, 20));
