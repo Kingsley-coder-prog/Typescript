a67582f237ec8aedb171dd313cef08e730b696c5
BlockGem <kifeanyi216@gmail.com>
2024-03-30 12:50:57 +0100

    Function Types & Callbacks

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -4,7 +4,14 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result is: ", num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(10, 20));
 var combinedValues;
 combinedValues = add;
 console.log(combinedValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -6,8 +6,17 @@ function printResult(num: number) {
   console.log("Result is: ", num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(10, 20));
 
 let combinedValues: (a: number, b: number) => number;
 combinedValues = add;
 console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, result => {
+  console.log(result);
+});
