0a9b565f53eb53a11c4ef17f52f2b8017aeb32cb
BlockGem <kifeanyi216@gmail.com>
2024-03-30 13:15:42 +0100

    The never type

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,17 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result is: ", num);
+// The "never" type
+function generateError(message, code) {
+    throw { message: message, error: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(10, 20));
-var combinedValues;
-combinedValues = add;
-console.log(combinedValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+generateError("An error occured", 500);
