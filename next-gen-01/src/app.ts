7d5b910ec7f83bfeff387521d4c43c0e9b8b2f7b
BlockGem <kifeanyi216@gmail.com>
2024-04-03 13:13:32 +0100

    Rest Parameters

--- next-gen-01/src/app.ts
+++ next-gen-01/src/app.ts
@@ -1,20 +1,20 @@
 // Code goes here!
 
-const add = (a: number, b: number = 5) => a + b;
+// const add = (a: number, b: number = 5) => a + b;
 
-const printOutput: (a: number | string) => void = output => console.log(output);
+// const printOutput: (a: number | string) => void = output => console.log(output);
 
-printOutput(add(5));
+// printOutput(add(5));
 
-// Spread Operator
+// // Spread Operator
 
-const hobbies = ["Hiking", "Swimming"];
+// const hobbies = ["Hiking", "Swimming"];
 
-const activeHobbies = ["Football"];
+// const activeHobbies = ["Football"];
 
-const myActive = activeHobbies.push(...hobbies);
-console.log(activeHobbies.push(...hobbies));
-console.log(myActive);
+// const myActive = activeHobbies.push(...hobbies);
+// console.log(activeHobbies.push(...hobbies));
+// console.log(myActive);
 
 const person = {
   userName: "Micky",
@@ -23,3 +23,12 @@ const person = {
 
 const Person = { ...person };
 console.log(Person);
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addNumbers = add(5, 4, 3, 3.7);
+console.log(addNumbers);
