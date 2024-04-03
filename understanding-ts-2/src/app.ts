04980b5b46afcf9e9e9e4d809aa10a979c9c602c
BlockGem <kifeanyi216@gmail.com>
2024-04-03 12:04:04 +0100

    Strict Compilation

--- understanding-ts-2/src/app.ts
+++ understanding-ts-2/src/app.ts
@@ -3,7 +3,11 @@
 // const userName = "Max";
 // console.log("My name is: ", userName);
 
-const click = document.querySelector("button")!;
-click.addEventListener("click", () => {
-  console.log("Cliked!");
-});
+const click = document.querySelector("button");
+function clickHandler(message: string) {
+  console.log("click me " + message);
+}
+
+if (click) {
+  click.addEventListener("click", clickHandler.bind(null, "Yes"));
+}
