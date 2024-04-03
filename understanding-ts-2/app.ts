5788cc180ac9b59bb691a7bcb874f103210acbf8
BlockGem <kifeanyi216@gmail.com>
2024-04-03 11:43:57 +0100

    Stop Emitting Files On Compilation-Error

--- understanding-ts-2/app.ts
+++
@@ -1,4 +0,0 @@
-console.log("Time to get started...");
-
-const userName = "Max";
-console.log("My name is: ", userName);
---
+++ understanding-ts-2/src/app.ts
@@ -0,0 +1,9 @@
+// console.log("Time to get started...");
+
+// const userName = "Max";
+// console.log("My name is: ", userName);
+
+const click = document.querySelector("button")!;
+click.addEventListener("click", () => {
+  console.log("Cliked!");
+});
