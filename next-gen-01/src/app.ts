8ffda400538e6b5c3ac91fbe5f7374a3c18f4395
BlockGem <kifeanyi216@gmail.com>
2024-04-03 13:00:03 +0100

    The Spread Operator

--- next-gen-01/src/app.ts
+++ next-gen-01/src/app.ts
@@ -5,3 +5,21 @@ const add = (a: number, b: number = 5) => a + b;
 const printOutput: (a: number | string) => void = output => console.log(output);
 
 printOutput(add(5));
+
+// Spread Operator
+
+const hobbies = ["Hiking", "Swimming"];
+
+const activeHobbies = ["Football"];
+
+const myActive = activeHobbies.push(...hobbies);
+console.log(activeHobbies.push(...hobbies));
+console.log(myActive);
+
+const person = {
+  userName: "Micky",
+  age: 34
+};
+
+const Person = { ...person };
+console.log(Person);
