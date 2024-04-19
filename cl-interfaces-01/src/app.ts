a1feea1c1a1043931435cca6e70dac88ce2a4c01
BlockGem <kifeanyi216@gmail.com>
2024-04-19 17:02:47 +0100

    Classes & Inheritance

--- cl-interfaces-01/src/app.ts
+++ cl-interfaces-01/src/app.ts
@@ -1,1 +1,12 @@
 // Code goes here!
+
+class Department {
+  name: string;
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const dental = new Department("Dental");
+
+console.log(dental);
