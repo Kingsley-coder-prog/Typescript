548f7914880edd2bb136b2ad193231924ddf19ae
BlockGem <kifeanyi216@gmail.com>
2024-04-20 17:53:37 +0100

    Class And This Basics

--- cl-interfaces-02-class-and-this-basics/src/app.ts
+++ cl-interfaces-02-class-and-this-basics/src/app.ts
@@ -4,13 +4,16 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
-  describe() {
-    console.log("Department: ", this.name);
+
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department('Accounting');
+
 accounting.describe();
 
-const accountingCopy = { describe: accounting.describe };
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
 accountingCopy.describe();
