02d49d1cd2a21f706b580c16a8e3bcf19d1126af
BlockGem <kifeanyi216@gmail.com>
2024-04-25 14:06:48 +0100

    New Folders Added

---
+++ cl-interfaces-12-implementing-interfaces/src/app.ts
@@ -0,0 +1,32 @@
+interface Greetable {
+  readonly name: string;
+  age: number;
+
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name: string;
+  age = 30;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person("Max");
+
+// user1 = {
+//   name: "Max",
+//   age: 20,
+
+//   greet(phrase: string) {
+//     console.log(phrase + " " + this.name);
+//   }
+// };
